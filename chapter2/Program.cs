b3fc79e32089d947f269502397909f479837c63b
DarkRiver.Veda <darkriver404@outlook.com>
2018-02-12 17:46:28 +0800

    chapter2

---
+++ chapter2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nx = 200;
+            int ny = 100;
+
+            vec3 v = new vec3();
+            StreamWriter sw = File.CreateText(Directory.GetCurrentDirectory() + "\\out.ppm");
+
+            sw.WriteLine("P3");
+            sw.WriteLine(nx.ToString() + " " + ny.ToString());
+            sw.WriteLine("255");
+            for (int j = ny - 1; j >= 0; j--)
+            {
+                for (int i = 0; i < nx; i++)
+                {
+                    v.set((i * 1.0f) / nx, (j * 1.0f) / ny, 0.2f);
+                    v *= 255.99f;
+                    v = v.GetIntVec();
+                    sw.WriteLine(v.ToString());
+                }
+            }
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
