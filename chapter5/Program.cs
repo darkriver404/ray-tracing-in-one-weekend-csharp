03d1ec4d1d3c57e9d9ec9c89b94bc21fd56dfdaf
DarkRiver.Veda <darkriver404@outlook.com>
2018-02-27 15:43:56 +0800

    chapter5

--- chapter5/Program.cs
+++ chapter5/Program.cs
@@ -1,24 +1,34 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
-namespace chapter4
+namespace chapter5
 {
     class Program
     {
-        static float CheckHitSphere(vec3 sphereCenter, float sphereRadius, ray r)//r = A+B*t
+        static bool HitSphere(vec3 sphereCenter, float sphereRadius, ray r, out float value)//r = A+B*t
         {
             vec3 oc = r.Origin - sphereCenter;
             float a = vec3.dot(r.Direction, r.Direction); //dot(B,B)
-            float b = 2 * vec3.dot(oc, r.Direction);      //2*dot(A-C,B)
+            float b = 2.0f * vec3.dot(oc, r.Direction);   //2*dot(A-C,B)
             float c = vec3.dot(sphereCenter, sphereCenter) - sphereRadius * sphereRadius; //dot(C,C)-R*R
             float discriminant = b * b - 4 * a * c;
-            return discriminant;
+            if (discriminant < 0)
+            {
+                value = -1;
+                return false;
+            }
+            value = (-b - (float)Math.Sqrt(discriminant)) / (2.0f * a);//closest(smallest) hit point
+            return true;
         }
+        
         static vec3 GetColor(ray r)
         {
-            float discriminant = CheckHitSphere(new vec3(0, 0, -1), 0.5f, r);
-            if(discriminant > 0)
+            float value = -1;
+            bool hit = HitSphere(new vec3(0, 0, -1), 0.5f, r, out value);
+            if (hit)
             {
-                return new vec3(1, 0, 0);//red
+                vec3 Normal = (r.point_at_parameter(value) - new vec3(0, 0, -1)).Normalize();//normal:[-1,1]
+                return (new vec3(1 + Normal.x, 1 + Normal.y, 1 + Normal.z) * 0.5f);//[-1,1]-->[0,2]-->[0,1]
             }
 
             vec3 normalizedDir = r.Direction.Normalize();
